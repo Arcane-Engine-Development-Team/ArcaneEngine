ef88214195e2cc19bfdcf85f4e307034ee954131
Hein Dekkers <heintjepeintje06@gmail.com>
2026-09-03 17:23:47 +0200

    Removed premake binary and duplicate liscence. Implemented windows memory allocation.

---
+++ Arcane.Core/Source/Platform/Windows/WindowsMemory.cpp
@@ -0,0 +1,34 @@
+#include <Arcane/Core/Core.hpp>
+
+#if defined(AR_PLATFORM_OS_WINDOWS)
+
+#include <Arcane/System/Memory.hpp>
+
+#include <windows.h>
+
+namespace Arcane {
+
+	void* allocate(u64 size, u64 alignment) {
+		if (size == 0) return nullptr;
+		if (alignment == 0) return nullptr;
+
+		void* pointer = HeapAlloc(
+			GetProcessHeap(),
+			0,
+			size);
+
+		return pointer;
+	}
+
+	void free(void* pointer) {
+		if (pointer == nullptr) return;
+
+		HeapFree(
+			GetProcessHeap(),
+			0,
+			pointer);
+	}
+
+}
+
+#endif // AR_PLATFORM_OS_WINDOWS
